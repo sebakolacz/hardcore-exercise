44054b157963fd531cfdc86d18d2657218dc0682
sebakolacz <70404904+sebakolacz@users.noreply.github.com>
2020-12-19 18:28:10 +0100

    html and styling ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,19 @@
-import * as mdb from 'mdb-ui-kit';
+// import * as mdb from 'mdb-ui-kit';
 
-export default {
-  mdb,
-};
+// export default {
+//   mdb,
+// };
+
+// const button = document.querySelector(".container__form-btn");
+// const toggleName = document.querySelector("#container__form-number-toggle");
+// const toggleKg = document.querySelector(".container__form-radio-input-kg");
+
+// if (toggleKg === checked) {
+//   toggleName.innerHTML = "Waga:";
+// } else {
+//   toggleName.innerText = "Liczba:";
+// }
+
+// button.addEventListener("click", e => {
+
+// });
