768c54f20a7bdc91008e3d491c78b95beb36d845
sebakolacz <70404904+sebakolacz@users.noreply.github.com>
2020-12-20 23:09:51 +0100

    script tests

--- src/js/index.js
+++ src/js/index.js
@@ -20,42 +20,91 @@ number.addEventListener("click", handleClick2);
 {
 
 const button = document.querySelector('.container__form-btn');
+// const ul = document.querySelector(".container__table-body-tr");
 
 button.addEventListener("click", e => {
   e.preventDefault();
 
-  // const el = document.createElement(tr);
-  
-  const numberCheck = document.querySelector('#szt');
-  const weightCheck = document.querySelector('#kg');
+  function addItem() {
+    const numberCheck = document.querySelector('#szt');
+    const weightCheck = document.querySelector('#kg');
 
-  const categoryInput = document.querySelector('.container__form-select-panel');
-  const category = document.querySelector('.category');
+    const categoryInput = document.querySelector('.container__form-select-panel');
+    const category = document.querySelector('.category');
 
-  const nameInput = document.querySelector('.container__form-input-name');
-  const name = document.querySelector('.name');
+    const nameInput = document.querySelector('.container__form-input-name');
+    const name = document.querySelector('.name');
 
-  const numberInput = document.querySelector('.container__form-input-number');
-  const number = document.querySelector('.number');
+    const numberInput = document.querySelector('.container__form-input-number');
+    const number = document.querySelector('.number');
 
-  const weightInput = document.querySelector('.container__form-input-number');
-  const weight = document.querySelector('.weight');
+    const weightInput = document.querySelector('.container__form-input-number');
+    const weight = document.querySelector('.weight');
 
-  category.innerHTML = categoryInput.value;
-  name.innerHTML = nameInput.value;
+    category.innerHTML = categoryInput.value;
+    name.innerHTML = nameInput.value;
 
-  if (numberCheck.checked) {
-    number.innerHTML = numberInput.value + " szt.";
-    weight.innerHTML = "--";
-  } else if (weightCheck.checked) {
-    weight.innerHTML = weightInput.value + " kg.";
-    number.innerHTML = "--";
+    if (numberCheck.checked) {
+      number.innerHTML = numberInput.value + " szt.";
+      weight.innerHTML = "--";
+    } else if (weightCheck.checked) {
+      weight.innerHTML = weightInput.value + " kg.";
+      number.innerHTML = "--";
+    }
   }
-  // number.innerHTML = numberInput.value;
-  // weight.innerHTML = weightInput.value;
-  // category.innerHTML = categoryInput.value;
-  // el.classList.add("tr");
-  // tr.append(el);
+  const newTr = document.createElement('tr');
+  // const newTh = document.createElement('th');
+  const newBtn = document.createElement('button');
+  const tr = document.querySelector(".container__table-tbody");
+  newTr.classList.add("container__table-body-tr");
+  // newTh.classList.add("container__table-body-th");
+  // newTr.append(newTh);
+  for(let i = 0; i < 1; i++){
+    for (let i = 0; i < 5; i++){
+      const newTh = document.createElement('th');
+      newTh.classList.add("container__table-body-th");
+      newTr.append(newTh);
+    };
+    const newBtn = document.createElement('button');
+    newBtn.classList.add("container__table-body-th-remove");
+    tr.append(newBtn);
+  };
+  tr.append(newTr);
+  // const newBtn = document.createElement('button');
+  // newBtn.classList.add("container__table-body-th-remove");
+  // tr.append(newBtn);
+
+  // newTr.append(newTh);
+  // newTr.append(newTh);
+  // newTr.append(newTh);
+  // newTr.append(newTh);
+  // newTr.append(newTh.append(newBtn));
+
+// addItem();
 });
 
 }
+
+{
+
+  const buttonRm = document.querySelector('.container__table-body-th-remove');
+  const trRm = document.querySelector('.container__table-body-tr');
+    buttonRm.forEach(el => {
+      el.addEventListener("click", function(e) {
+        el.preventDefault();
+        el.closest('tr').remove();
+      })
+    })
+
+}
+// {
+
+//   const buttonRm = document.querySelector('.container__table-body-th-remove');
+//   const tr = document.querySelector('.container__table-body-tr');
+
+//     buttonRm.addEventListener("click", function(e) {
+//       // e.preventDefault();
+//       tr.remove();
+//     })
+
+// }
