9b80389dea667ff41a9717b23ed3d8b5602927ab
sebakolacz <70404904+sebakolacz@users.noreply.github.com>
2020-12-22 22:45:41 +0100

    summary almost ready

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
   button.addEventListener('click', (e) => {
     e.preventDefault();
 
+    // creating tbody
+
     const newTrElement = document.createElement('tr');
     const tbody = document.querySelector('.table-tbody');
     newTrElement.classList.add('table-body-tr');
-    tbody.append(newTrElement);
+    tbody.insertBefore(newTrElement, newTrSummary);
 
     const newTdElementCategory = document.createElement('td');
     newTdElementCategory.classList.add('table-body-td-category');
@@ -20,6 +22,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const newTdElementNumber = document.createElement('td');
     newTdElementNumber.classList.add('table-body-td-number');
     newTrElement.append(newTdElementNumber);
+
+    newTdElementNumberSummary.textContent = newTdElementNumber.value;
 
     const newTdElementWeight = document.createElement('td');
     newTdElementWeight.classList.add('table-body-td-weight');
@@ -53,11 +57,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const literInput = document.querySelector('.form-input-number');
 
+    // from input to table and local storage
+
     localStorage.setItem('category', categoryInput.value);
     localStorage.setItem('name', nameInput.value);
-    localStorage.setItem('number', numberInput.value);
-    localStorage.setItem('weight', weightInput.value);
-    localStorage.setItem('liter', literInput.value);
 
     newTdElementCategory.textContent = categoryInput.value;
     newTdElementName.textContent = nameInput.value;
@@ -79,17 +82,23 @@ document.addEventListener('DOMContentLoaded', () => {
       localStorage.setItem('liter', literInput.value);
     }
 
+    // clearing inputs
+
     categoryInput.value = '';
     nameInput.value = '';
     numberInput.value = '';
     weightInput.value = '';
     literInput.value = '';
 
+    // removing tr
+
     newButtonElement.addEventListener('click', (e) => {
       e.preventDefault();
       newTrElement.remove();
     });
   });
+
+  // from local storage to table
 
   const categoryInput = document.querySelector('.form-select-panel');
 
@@ -136,10 +145,6 @@ document.addEventListener('DOMContentLoaded', () => {
   newTdElementButton.append(newButtonElement);
   newButtonElement.textContent = 'Usuń';
 
-  const numberCheck = document.querySelector('#szt');
-  const weightCheck = document.querySelector('#kg');
-  const literCheck = document.querySelector('#l');
-
   const valueOfItemCategory = localStorage.getItem('category', categoryInput.value);
   const valueOfItemName = localStorage.getItem('name', nameInput.value);
   const valueOfItemNumber = localStorage.getItem('number', numberInput.value);
@@ -147,21 +152,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const valueOfItemLiter = localStorage.getItem('liter', literInput.value);
 
   newTdElementCategory.textContent = valueOfItemCategory;
+
   newTdElementName.textContent = valueOfItemName;
 
-  if (numberCheck.checked) {
-    newTdElementNumber.textContent = `${valueOfItemNumber} szt.`;
-    newTdElementWeight.textContent = '--';
-    newTdElementLiter.textContent = '--';
-  } else if (weightCheck.checked) {
-    newTdElementWeight.textContent = `${valueOfItemWeight} kg.`;
-    newTdElementNumber.textContent = '--';
-    newTdElementLiter.textContent = '--';
-  } else if (literCheck.checked) {
-    newTdElementLiter.textContent = `${valueOfItemLiter} l.`;
-    newTdElementWeight.textContent = '--';
-    newTdElementNumber.textContent = '--';
-  }
+  newTdElementNumber.textContent = `${valueOfItemNumber} szt.`;
+
+  newTdElementWeight.textContent = `${valueOfItemWeight} kg.`;
+
+  newTdElementLiter.textContent = `${valueOfItemLiter} l.`;
+
+  // removing data in local storage
 
   newButtonElement.addEventListener('click', (e) => {
     e.preventDefault();
@@ -173,8 +173,52 @@ document.addEventListener('DOMContentLoaded', () => {
     localStorage.removeItem('liter');
   });
 
+  // printing table
+
   const print = document.querySelector('.print-table');
   print.addEventListener('click', () => {
     window.print();
   });
+
+  // creating summary tr
+
+  const newTrSummary = document.createElement('tr');
+  newTrSummary.classList.add('table-body-tr-summary');
+  tbody.append(newTrSummary);
+
+  const newTdElementCategorySummary = document.createElement('td');
+  newTdElementCategorySummary.classList.add('table-body-td-category-summary');
+  newTrSummary.append(newTdElementCategorySummary);
+  newTdElementCategorySummary.textContent = 'Łącznie:';
+
+  const newTdElementNameSummary = document.createElement('td');
+  newTdElementNameSummary.classList.add('table-body-td-name-summary');
+  newTrSummary.append(newTdElementNameSummary);
+
+  const newTdElementNumberSummary = document.createElement('td');
+  newTdElementNumberSummary.classList.add('table-body-td-number-summary');
+  newTrSummary.append(newTdElementNumberSummary);
+
+  newTdElementNumberSummary.textContent = newTdElementNumber.value;
+
+  const newTdElementWeightSummary = document.createElement('td');
+  newTdElementWeightSummary.classList.add('table-body-td-weight-summary');
+  newTrSummary.append(newTdElementWeightSummary);
+
+  const newTdElementLiterSummary = document.createElement('td');
+  newTdElementLiterSummary.classList.add('table-body-td-liter-summary');
+  newTrSummary.append(newTdElementLiterSummary);
+
+  const newTdElementClearSpace = document.createElement('td');
+  newTdElementClearSpace.classList.add('table-body-td-button-summary');
+  newTrSummary.append(newTdElementClearSpace);
+
+  // delated cleared tr
+
+  const addClearTrWalidation = () => {
+    if (localStorage.getItem('category') === null) {
+      newTrElement.remove();
+    }
+  };
+  addClearTrWalidation();
 });
